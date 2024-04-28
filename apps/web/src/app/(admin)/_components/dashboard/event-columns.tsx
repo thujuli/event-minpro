fa93d659705ca26b681b72004a97246047da42e1
thujuli <thujuli037@gmail.com>
2024-04-28 14:26:57 +0700

    feat: create table to show dummy data on dashboard
    components
    - breadcrumb-dashboard
    - event-columns
    - profile-options
    - data-table
    - data-table-pagination
    - data-table-column-header

---
+++ apps/web/src/app/(admin)/_components/dashboard/event-columns.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { User } from "@/types/users";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { DataTableColumnHeader } from "@/components/ui/data-table-column-header";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export const eventColumns: ColumnDef<User>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return <DataTableColumnHeader column={column} title="Email" />;
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+      return <DataTableColumnHeader column={column} title="Created At" />;
+    },
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("createdAt"));
+      const formatted = date.toLocaleDateString();
+      return <div className="font-bold">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const name = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(name.id)}
+            >
+              Copy name ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View name details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- apps/web/src/app/(admin)/dashboard/page.tsx
+++ apps/web/src/app/(admin)/dashboard/page.tsx
@@ -1,7 +1,21 @@
 import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
 import { PlusCircle } from "lucide-react";
+import { eventColumns } from "../_components/dashboard/event-columns";
 
-const DashboardPage: React.FC = () => {
+// dummy data
+const getData = async () => {
+  const res = await fetch(
+    "https://662dd263a7dda1fa378b5b89.mockapi.io/api/users",
+  );
+  const json = await res.json();
+  return json;
+};
+
+const DashboardPage: React.FC = async () => {
+  // fetch dummy data
+  const data = await getData();
+
   return (
     <>
       <div>
@@ -12,19 +26,20 @@ const DashboardPage: React.FC = () => {
           </span>
         </Button>
       </div>
-      <div
-        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-        x-chunk="dashboard-02-chunk-1"
-      >
-        <div className="flex flex-col items-center gap-1 text-center">
+      <div className="flex flex-1 items-center justify-center">
+        <div className="w-full">
+          <DataTable columns={eventColumns} data={data} />
+        </div>
+
+        {/* <div className="flex flex-col items-center gap-1 text-center">
           <h3 className="text-2xl font-bold tracking-tight">
             You have no events
           </h3>
           <p className="text-sm text-muted-foreground">
-            You can start selling as soon as you add a product.
+            Set up an event and proceed to sell tickets.
           </p>
           <Button className="mt-4">Add Event</Button>
-        </div>
+        </div> */}
       </div>
     </>
   );
