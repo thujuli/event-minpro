392bc68932db68f562b147fdfbb40d9d7c92bf8a
I Putu Julianta <95893131+thujuli@users.noreply.github.com>
2024-05-04 03:34:36 +0700

    feat: seed users with prisma (#18)

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,16 +1,36 @@
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 import { locations } from './data/locations';
 import { categories } from './data/categories';
+import { users } from './data/users';
+import { SALT } from '../src/config';
 
 const prisma = new PrismaClient();
 
 async function seed() {
+  const hashPassword = (password: string) => {
+    const salt = bcrypt.genSaltSync(Number(SALT));
+    return bcrypt.hashSync(password, salt);
+  };
+
   for (const location of locations) {
     await prisma.location.create({ data: location });
   }
 
   for (const category of categories) {
     await prisma.category.create({ data: category });
+  }
+
+  for (const user of users) {
+    await prisma.user.create({
+      data: {
+        username: user.username,
+        email: user.email,
+        password: hashPassword(user.password),
+        isAdmin: user.isAdmin,
+        referralCode: user.referralCode,
+      },
+    });
   }
 }
 
