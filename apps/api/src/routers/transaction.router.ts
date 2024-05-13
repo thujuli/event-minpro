b8d97f2b644e24329ec2e11e39098a30395bf10e
aryadmara <158138683+aryadmara@users.noreply.github.com>
2024-05-13 13:00:46 +0700

    create api voucher (#51)
    * feat: create api voucher
    
    * feat: create api review, pahe review success and waiting
    
    * feat: resolve conflict v2

--- apps/api/src/routers/transaction.router.ts
+++ apps/api/src/routers/transaction.router.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { ReviewController } from '@/controllers/review.controller';
 import { TransactionController } from '@/controllers/transaction.controller';
 import { adminGuard, userGuard, verifyToken } from '@/middlewares/auth.middleware';
@@ -11,45 +11,33 @@ export class TrasactionRouter {
   constructor() {
     this.router = Router();
     this.trasactionController = new TransactionController();
-=======
-import { TransactionController } from '@/controllers/transaction.controller';
-import { userGuard, verifyToken } from '@/middlewares/auth.middleware';
-import { Router } from 'express';
-
-export class TransactionRouter {
-  private router: Router;
-  private transactionController: TransactionController;
-
-  constructor() {
-    this.router = Router();
-    this.transactionController = new TransactionController();
->>>>>>> main
     this.initializeRoutes();
   }
 
   private initializeRoutes(): void {
-<<<<<<< HEAD
     this.router.get(
       '/waiting',
       verifyToken,
-      this.trasactionController.getEventTransactionWaiting,
+      this.transactionController.getEventTransactionWaiting,
     );
+
     this.router.get(
       '/success',
       verifyToken,
-      this.trasactionController.getEventTransactionSuccess,
+      this.transactionController.getEventTransactionSuccess,
     );
+
     this.router.get(
       '/finish',
       verifyToken,
-      this.trasactionController.getEventTransactionSuccessByDate,
-=======
+      this.transactionController.getEventTransactionSuccessByDate,
+    );
+
     this.router.post(
       '/',
       verifyToken,
       userGuard,
       this.transactionController.createTransaction,
->>>>>>> main
     );
   }
 
